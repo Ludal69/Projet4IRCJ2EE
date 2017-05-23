4cf624c183eb30eabf1d351b48530c797e510e73
A208280 <ludovic.gorron@volvo.com>
2017-05-23 15:57:59 +0200

    web.xml

--- src/com/projet/controler/bean/Test.java
+++ src/com/projet/controler/bean/Test.java
@@ -1,5 +1,23 @@
 package com.projet.controler.bean;
 
-public class Test {
- int bonjour;
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+
+
+@ManagedBean
+@RequestScoped
+public class Test implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String            nom;
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom( String nom ) {
+        this.nom = nom;
+    }
 }
