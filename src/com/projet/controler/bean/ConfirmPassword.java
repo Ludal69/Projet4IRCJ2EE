bcde02a2f18a7c2e86db528b01b40ceb6edbd446
Florian Croisot <florian.croisot@gmail.com>
2017-05-28 23:50:28 +0200

    Validation password (Marche pas)

---
+++ src/com/projet/controler/bean/ConfirmPassword.java
@@ -0,0 +1,46 @@
+package com.projet.controler.bean;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+
+public class ConfirmPassword implements Serializable{
+	private String submittedValue;
+	private String confirmedValue;
+	
+	public ConfirmPassword(){
+		
+	}
+	
+	
+
+	public ConfirmPassword(String submittedValue, String confirmedValue) {
+		this.submittedValue = submittedValue;
+		this.confirmedValue = confirmedValue;
+	}
+
+
+
+	public String getSubmittedValue() {
+		return submittedValue;
+	}
+
+	public void setSubmittedValue(String submittedValue) {
+		this.submittedValue = submittedValue;
+	}
+
+	public String getConfirmedValue() {
+		return confirmedValue;
+	}
+
+	public void setConfirmedValue(String confirmedValue) {
+		this.confirmedValue = confirmedValue;
+	}
+	
+	
+
+}
