7c61ed3975db2d98c2ffb491f6bca48edf700260
Florian Croisot <florian.croisot@gmail.com>
2017-05-30 20:50:46 +0200

    BDD mise à jour

---
+++ WebContent/projet4irc_jee.sql
@@ -0,0 +1,128 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Client :  127.0.0.1
+-- Généré le :  Mar 30 Mai 2017 à 20:48
+-- Version du serveur :  5.6.17
+-- Version de PHP :  5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données :  `projet4irc_jee`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ingrediant`
+--
+
+CREATE TABLE IF NOT EXISTS `ingrediant` (
+  `ingrediant_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `description` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`ingrediant_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=7 ;
+
+--
+-- Contenu de la table `ingrediant`
+--
+
+INSERT INTO `ingrediant` (`ingrediant_id`, `name`, `description`) VALUES
+(1, 'Riz', NULL),
+(2, 'Petits poids', NULL),
+(3, 'Jambon', NULL),
+(4, 'Oeufs en omelette', NULL),
+(5, 'Steak haché ', NULL),
+(6, 'Frites', 'Bien Gras');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `recipe`
+--
+
+CREATE TABLE IF NOT EXISTS `recipe` (
+  `recipe_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `description` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`recipe_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+
+--
+-- Contenu de la table `recipe`
+--
+
+INSERT INTO `recipe` (`recipe_id`, `name`, `description`) VALUES
+(1, 'Riz cantonais', NULL),
+(2, 'Steak frites', 'Pour bien grossir');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `recipelinkingrediant`
+--
+
+CREATE TABLE IF NOT EXISTS `recipelinkingrediant` (
+  `ingrediant_id` int(11) NOT NULL,
+  `recipe_id` int(11) NOT NULL,
+  PRIMARY KEY (`ingrediant_id`,`recipe_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Contenu de la table `recipelinkingrediant`
+--
+
+INSERT INTO `recipelinkingrediant` (`ingrediant_id`, `recipe_id`) VALUES
+(1, 1),
+(2, 1),
+(3, 1),
+(4, 1),
+(5, 2),
+(6, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `pwd` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `firstname` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `lastname` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `email` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `isconnected` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=11 ;
+
+--
+-- Contenu de la table `user`
+--
+
+INSERT INTO `user` (`user_id`, `login`, `pwd`, `firstname`, `lastname`, `age`, `email`, `isconnected`) VALUES
+(1, 'az', 'az', '', '', 0, 'az', NULL),
+(2, 'ze', 'ze', 'ze', 'ze', 82, 'aze', NULL),
+(3, 'sdcx', 'zqdsx', 'az', 'zedsfxc', 0, 'zd<esfvxc', NULL),
+(4, 'flo', 'flo', 'flo', 'flo', 8, 'flo@flo.com', NULL),
+(5, 'tr', '7', 't', 'r', 48, 'er@ju.gt', NULL),
+(7, 'azert', 'za', 'er', 'er', 7, 'ae@rf.fr', NULL),
+(8, 'az', 'z', 'az', 'az', 0, 'az@truc.fr', NULL),
+(9, 'zs', 'se', 'azaz', 'zeze', 0, 'as@er.com', NULL),
+(10, 'ds', 'go', 'assqx', 'az', 7, 'az@cpe.fr', NULL);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
