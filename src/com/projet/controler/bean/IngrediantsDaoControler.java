189127b228222d120d71c0f3823d1bf7b7015f88
Florian Croisot <florian.croisot@gmail.com>
2017-05-31 13:29:50 +0200

    Marche pas

--- src/com/projet/controler/bean/IngrediantsDaoControler.java
+++ src/com/projet/controler/bean/IngrediantsDaoControler.java
@@ -9,6 +9,10 @@ import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
 
 import com.projet.dao.IngrediantsDao;
 import com.projet.model.bean.Ingrediant;
@@ -17,39 +21,49 @@ import com.projet.model.bean.Recipe;
 @SuppressWarnings("serial")
 @ManagedBean
 @ApplicationScoped
+@FacesConverter("myObjectConverter")
 
 public class IngrediantsDaoControler implements Serializable{
 	private IngrediantsDao ingrediantsDao;
-	private Recipe recipe;
+	//private Recipe recipe;
+	private String recipeName;
 	private List<Ingrediant> ingrediants;
 	
 	
 	
 	public IngrediantsDaoControler() {
 		this.ingrediantsDao = new IngrediantsDao();
-		this.recipe = new Recipe();
+		this.recipeName = "Test";
 		this.ingrediants = new ArrayList<Ingrediant>();
+	}
+
+	public String getRecipeName() {
+		return recipeName;
+	}
+
+	public void setRecipeName(String recipeName) {
+		this.recipeName = recipeName;
 	}
 
 	public List<Ingrediant> getIngrediants(){
 		//For test
 		//this.recipe.setName("Steak frites");//For test
-		System.out.println(this.recipe.getName());
-		this.ingrediants = this.ingrediantsDao.getListIngrediants(this.recipe);
+		System.out.println(this.recipeName);
+		this.ingrediants = this.ingrediantsDao.getListIngrediants(this.recipeName);
 		return this.ingrediants;
 	}
 
 
 
-	public Recipe getRecipe() {
-		return recipe;
-	}
-
-
-
-	public void setRecipe(Recipe recipe) {
-		this.recipe = recipe;
-	}
+//	public Recipe getRecipe() {
+//		return recipe;
+//	}
+//
+//
+//
+//	public void setRecipe(Recipe recipe) {
+//		this.recipe = recipe;
+//	}
 	
 	
 	
---
+++ src/com/projet/controler/bean/RecipeConverter.java
@@ -0,0 +1,38 @@
+package com.projet.controler.bean;
+
+import java.util.Map;
+
+import javax.el.ValueExpression;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+import javax.faces.view.facelets.ConverterConfig;
+import javax.faces.view.facelets.FaceletHandler;
+import javax.faces.view.facelets.Tag;
+
+import com.projet.model.bean.Recipe;
+
+@FacesConverter(value = "recipeConverter")
+public class RecipeConverter implements Converter {
+
+	@Override
+	public Object getAsObject(FacesContext ctx, UIComponent arg1, String recipeName) {
+		System.out.println(recipeName);
+		ValueExpression vex =
+                ctx.getApplication().getExpressionFactory()
+                        .createValueExpression(ctx.getELContext(),
+                                "#{recepiesDaoControler}", RecipesDaoControler.class);
+		 Map<String, Object> context = FacesContext.getCurrentInstance().getExternalContext().getApplicationMap();
+		 RecipesDaoControler recipesDaoControler = (RecipesDaoControler) context.get("recipesDaoControler");
+        return recipesDaoControler.getRecipe(recipeName);
+	}
+
+	@Override
+	public String getAsString(FacesContext arg0, UIComponent arg1, Object recipe) {
+		return (String)recipe.getClass().getName();
+	}
+	
+	
+
+}
