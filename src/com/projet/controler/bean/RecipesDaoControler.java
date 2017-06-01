176ec88d3c7c742aa416de920aee7577cb09be98
A208280 <ludovic.gorron@volvo.com>
2017-06-01 20:01:37 +0200

    DAO RECIPE OK AVEC REQUETE PUTAIN

--- src/com/projet/controler/bean/RecipesDaoControler.java
+++ src/com/projet/controler/bean/RecipesDaoControler.java
@@ -2,6 +2,7 @@ package com.projet.controler.bean;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
@@ -14,7 +15,7 @@ import com.projet.dao.RecipesDao;
 import com.projet.model.bean.Recipe;
 
 @ManagedBean
-@ViewScoped
+@SessionScoped
 public class RecipesDaoControler implements Serializable {
     /**
 	 * 
@@ -22,30 +23,70 @@ public class RecipesDaoControler implements Serializable {
     private static final long serialVersionUID = 1L;
     private RecipesDao recipesDao;
     private List<Recipe> recipes;
+    
+    private int durationWanted = 0;
+    private int level = 0;
+    private int numberPeople = 0;
+    private String type = "";
 
     public RecipesDaoControler() {
         this.recipesDao = new RecipesDao();
         this.recipes = new ArrayList<Recipe>();
     }
 
-
     public List<Recipe> getRecipes() {
         this.recipes = this.recipesDao.getListRecipes();
-        System.out.println(recipes.get(2).getName());
         return recipes;
     }
 
-//    public Recipe getRecipe(String name) {
-//        Recipe recipe = null;
-//        System.out.println(name);
-//        for (Recipe i : this.recipes) {
-//            System.out.println(i.getClass().getName());
-//            if (i.getName().equals(name)) {
-//                recipe = (Recipe) i;
-//            }
-//        }
-//        return recipe;
-//    }
+    public String searchRecipe() {
+        this.recipes =  this.recipesDao.searchRecipe(getDurationWanted(), getLevel(), getNumberPeople(), getType());
+        return "splashScreen.jsf";
+    }
 
+    // TEST
+    public Recipe getRecipe(String name) {
+        Recipe recipe = null;
+        System.out.println(name);
+        for (Recipe i : this.recipes) {
+            System.out.println(i.getClass().getName());
+            if (i.getName().equals(name)) {
+                recipe = (Recipe) i;
+            }
+        }
+        return recipe;
+    }
+
+    public int getDurationWanted() {
+        return durationWanted;
+    }
+
+    public void setDurationWanted(int durationWanted) {
+        this.durationWanted = durationWanted;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getNumberPeople() {
+        return numberPeople;
+    }
+
+    public void setNumberPeople(int numberPeople) {
+        this.numberPeople = numberPeople;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 
 }
--- src/com/projet/dao/RecipesDao.java
+++ src/com/projet/dao/RecipesDao.java
@@ -1,53 +1,99 @@
 package com.projet.dao;
 
+import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
+
 import com.projet.model.bean.Ingrediant;
 import com.projet.model.bean.Recipe;
+import com.projet.model.bean.User;
 
-public class RecipesDao extends AbstractDao{
-	
-	public RecipesDao(){
-		super();
-	}
-	
-	public List<Recipe> getListRecipes(){
-		List<Recipe> recipes = new ArrayList<Recipe>();
-		ResultSet resultSet = null;
-		
-		try {
-			java.sql.Connection cnx = java.sql.DriverManager.getConnection(
-					this.DB_CONFIG, this.DB_LOGIN, this.DB_PWD);
-			// Cr�ation de la requ�te
-			java.sql.Statement query = cnx.createStatement();
+public class RecipesDao extends AbstractDao {
 
-			// Cr�ation du contenu de la requ�te a �x�cuter
-			String sql ="SELECT * FROM `recipe`";
+    public RecipesDao() {
+        super();
+    }
 
-			// Ex�cution de la requ�te de modification
-			//query.executeUpdate(sql);
-			
-			resultSet = query.executeQuery(sql);
-			
-			while (resultSet.next()) {
-				Recipe recipe = new Recipe();
-				recipe.setName(resultSet.getString("name"));
-				recipe.setDescription(resultSet.getString("description"));
-				recipes.add(recipe);
-			}
+    public List<Recipe> getListRecipes() {
+        List<Recipe> recipes = new ArrayList<Recipe>();
+        ResultSet resultSet = null;
 
-//			cnx.commit();
-			query.close();
-			cnx.close();
+        try {
+            java.sql.Connection cnx = java.sql.DriverManager.getConnection(this.DB_CONFIG, this.DB_LOGIN, this.DB_PWD);
+            // Cr�ation de la requ�te
+            java.sql.Statement query = cnx.createStatement();
 
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
-		return recipes;
-	}
+            // Cr�ation du contenu de la requ�te a �x�cuter
+            String sql = "SELECT * FROM `recipe`";
+
+            // Ex�cution de la requ�te de modification
+            // query.executeUpdate(sql);
+
+            resultSet = query.executeQuery(sql);
+
+            while (resultSet.next()) {
+                Recipe recipe = new Recipe();
+                recipe.setName(resultSet.getString("name"));
+                recipe.setDuration(resultSet.getInt("duration"));
+                recipe.setLevel(resultSet.getInt("level"));
+                recipe.setNumberPeople(resultSet.getInt("number_people"));
+                recipe.setType(resultSet.getString("type"));
+                recipes.add(recipe);
+            }
+
+            // cnx.commit();
+            query.close();
+            cnx.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return recipes;
+    }
+
+    public List<Recipe> searchRecipe(int durationWanted, int level, int numberPeople, String type) {
+        // TODO Auto-generated method stub
+        List<Recipe> recipes = new ArrayList<Recipe>();
+        ResultSet resultSet = null;
+
+        try {
+            java.sql.Connection cnx = java.sql.DriverManager.getConnection(this.DB_CONFIG, this.DB_LOGIN, this.DB_PWD);
+            // Cr�ation de la requ�te
+            java.sql.Statement query = cnx.createStatement();
+
+            // Cr�ation du contenu de la requ�te a �x�cuter
+             String sql = "SELECT * FROM `recipe` WHERE `duration` = " + durationWanted + " AND `level` = " + level + " AND `number_people` = " + numberPeople
+             + " AND type LIKE '"+type+"';";
+
+            // Ex�cution de la requ�te de modification
+            // query.executeUpdate(sql);
+
+            resultSet = query.executeQuery(sql);
+
+            while (resultSet.next()) {
+                Recipe recipe = new Recipe();
+                recipe.setName(resultSet.getString("name"));
+                recipe.setDuration(resultSet.getInt("duration"));
+                recipe.setLevel(resultSet.getInt("level"));
+                recipe.setNumberPeople(resultSet.getInt("number_people"));
+                recipe.setType(resultSet.getString("type"));
+                recipes.add(recipe);
+            }
+
+            // cnx.commit();
+            query.close();
+            cnx.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return recipes;
+    }
 }
-
